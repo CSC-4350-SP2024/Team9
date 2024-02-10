654ba1ce43e5530ea11f65404def2c8fdb9c5d5a
valentinaaranggo <valea28@gmail.com>
2024-02-10 08:48:10 -0500

    created the react app structure where I created components such as login and the signup forms then created a navbar compoenent in order to link those components by using react router. Also implemented frameworks such as tailwind and implemented google fonts services.

---
+++ study-buddy-app/src/components/Signup.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Signup = () => {
+    return (
+        <div className="w-full h-lvh pt-12 bg-slate-50">
+            <h1 className="text-3xl font-semibol text-center text-black">Sign Up</h1>
+            <form className="mt-6">
+                <div className="mb-2">
+                    <label className="block text-sm font-semibold text-center text-black">Username:</label>
+                    <div className="flex justify-center">
+                        <input type="text" id="name" placeholder="Enter" className="block w-64 px-4 py-2 mt-2 text-black bg-white border-2 border-black rounded-xl focus:border-purple-400 focus:ring-purple-300 focus:outline-none " required />
+                        </div>
+                </div>
+                 <div className="mb-2">
+                    <label className="block text-sm font-semibold text-center text-black">Email:</label>
+                    <div className="flex justify-center">
+                        <input type="text" id="name" placeholder="Enter" className="block w-64 px-4 py-2 mt-2 text-black bg-white border-2 border-black rounded-xl focus:border-purple-400 focus:ring-purple-300 focus:outline-none " required />
+                        </div>
+                </div>
+                 <div className="mb-2">
+                    <label className="block text-sm font-semibold text-center text-black">Password:</label>
+                    <div className="flex justify-center">
+                        <input type="text" id="name" placeholder="Enter" className="block w-64 px-4 py-2 mt-2 text-black bg-white border-2 border-black rounded-xl focus:border-purple-400 focus:ring-purple-300 focus:outline-none " required />
+                        </div>
+                </div>
+            </form> 
+        </div>
+    );
+};
+
+export default Signup;
