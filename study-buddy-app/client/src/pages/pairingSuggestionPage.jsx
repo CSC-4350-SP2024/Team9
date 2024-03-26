c9d1ea5b03713199b62e894b35cc551bf394bafc
roycel6 <129793658+roycel6@users.noreply.github.com>
2024-03-26 12:56:06 -0400

    added pairingsuggestionpage

---
+++ study-buddy-app/client/src/pages/pairingSuggestionPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PairingSuggestion from '../components/pairing/pairingSuggestion.jsx'
+
+const pairingSuggestionPage = () => {
+  return (
+    <div><PairingSuggestion/></div>
+  )
+}
+
+export default pairingSuggestionPage
