26ca2ec1029a70b9e42a9b62a6d0987e2db289e7
roycel6 <129793658+roycel6@users.noreply.github.com>
2024-04-15 18:35:52 -0400

    added message routes

--- study-buddy-app/server/routes/index.js
+++ study-buddy-app/server/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const userRoutes = require("./userRoutes");
+const messageRoutes = require("./messageRoutes");
 
 const router = express.Router();
 
 // Mount user routes
 router.use("/api", userRoutes);
+router.use("/api", messageRoutes);
 
 module.exports = router;
