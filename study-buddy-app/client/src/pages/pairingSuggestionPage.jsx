1e1acec7f53fb518485e0c042f6e8fd3e8e410bc
roycel6 <129793658+roycel6@users.noreply.github.com>
2024-04-16 22:12:57 -0400

    updated styling

--- study-buddy-app/client/src/pages/pairingSuggestionPage.jsx
+++ study-buddy-app/client/src/pages/pairingSuggestionPage.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PairingSuggestion from '../components/pairing/pairingSuggestion.jsx'
 
-const pairingSuggestionPage = () => {
+const PairingSuggestionPage = () => {
+  const [isAdded, setIsAdded] = useState(false);
+
+  const handleIsAdded = () => {
+    setIsAdded(!isAdded);
+  }
+  
   return (
-    <div><PairingSuggestion/></div>
+     <div className="flex flex-col items-center bg-slate-100 h-screen">
+      <h2 className="text-2xl font-bold text-gray-900 my-8">Matches</h2>
+
+        <div className="w-full max-w-md">
+
+            <div className="bg-white my-4 p-4 rounded shadow-md w-full flex justify-between items-center">
+              <p className="text-lg font-semibold">Friend 1</p>
+          <div className="flex space-x-4">
+            {!isAdded ? (
+              < button
+                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+              onClick = {handleIsAdded}>Add</button>
+            ) : (
+                <button
+                  className="px-4 py-2 bg-blue-700 text-white rounded-md hover:bg-blue-600"
+                  onClick={handleIsAdded}
+                >Added</button>
+            )}
+
+              </div>
+            </div>
+
+        </div>
+
+    </div>
   )
 }
 
-export default pairingSuggestionPage
+export default PairingSuggestionPage
