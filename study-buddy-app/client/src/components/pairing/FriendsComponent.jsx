9550de7556ef330d2ae8d7127cbc8252ca0f1547
roycel6 <129793658+roycel6@users.noreply.github.com>
2024-04-18 14:35:04 -0400

    updated friend system

--- study-buddy-app/client/src/components/pairing/FriendsComponent.jsx
+++ study-buddy-app/client/src/components/pairing/FriendsComponent.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const FriendsComponent = ({ friends }) => {
+const FriendsComponent = ({ friends, classmates, setClassmates }) => {
 
-  const handleRemoveClass = async (friendId) => {
+  const handleRemoveFriend = async (friendId) => {
     try {
       const response = await fetch('/api/removeFriend', {
         method: 'DELETE',
@@ -11,6 +11,13 @@ const FriendsComponent = ({ friends }) => {
         },
         body: JSON.stringify({ friendId }),
       });
+      const userResponse = await fetch('/api/user');
+      const userData = await handleResponse(userResponse);
+
+      const classmatesResponse = await fetch('/api/classmates');
+      const classmatesData = await handleResponse(classmatesResponse);
+      const filteredClassmates = classmatesData.classmates.filter(classmate => classmate.id !== userData.user?.id);
+      setClassmates(filteredClassmates);
       if (!response.ok) {
         throw new Error('Failed to remove friend');
       }
@@ -18,6 +25,15 @@ const FriendsComponent = ({ friends }) => {
       console.error(error);
     }
   };
+
+  const handleResponse = async (response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
+    }
+  };
+
   return (
     <div>
       <h2 className="text-2xl font-bold text-gray-900 my-8">Friends</h2>
@@ -29,7 +45,7 @@ const FriendsComponent = ({ friends }) => {
             {friend.friend.username}</h3>
             <li>Phone Number: {friend.friend.phone_number}</li>
             <li>Discord: {friend.friend.discord_name}</li>
-            <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600" onClick={() => handleRemoveClass(friend.friend.id)}>Delete</button></div></>
+            <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600" onClick={() => handleRemoveFriend(friend.friend.id)}>Delete</button></div></>
         ))}
       </ul>
     </div>
--- study-buddy-app/client/src/components/pairing/PendingRequestsComponent.jsx
+++ study-buddy-app/client/src/components/pairing/PendingRequestsComponent.jsx
@@ -1,14 +1,71 @@
 import React from 'react';
 
-const PendingRequestsComponent = ({ pendingRequests }) => {
+const PendingRequestsComponent = ({ pendingRequests, classmates, setClassmates }) => {
+
+  const handleAddFriend = async (senderId, requestId) => {
+    try {
+      const response = await fetch('/api/acceptFriendRequest', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ senderId, requestId }), 
+      });
+      setClassmates(classmates.filter(classmate => classmate.id !== senderId));
+      if (!response.ok) {
+        throw new Error('Failed to add friend');
+
+      } 
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleRejectRequest = async (requestId, senderId) => {
+    try {
+      const response = await fetch('/api/rejectFriendRequest', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ requestId }),
+      });
+      const userResponse = await fetch('/api/user');
+      const userData = await handleResponse(userResponse);
+
+      const classmatesResponse = await fetch('/api/classmates');
+      const classmatesData = await handleResponse(classmatesResponse);
+      const filteredClassmates = classmatesData.classmates.filter(classmate => classmate.id !== userData.user?.id);
+      setClassmates(filteredClassmates);
+
+      if (!response.ok) {
+        throw new Error('Failed to reject request');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+ 
+  const handleResponse = async (response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
+    }
+  };
+  
   return (
     <div>
       <h2 className="text-xl font-bold mt-8 mb-2">Pending Requests</h2>
       <ul className="w-full max-w-md">
         {pendingRequests.map(request => (
-          <li key={request.id} className="bg-white my-2 p-4 rounded shadow-md">
-            {request.sender ? `${request.sender.username} - Pending` : 'Unknown Sender'}
-          </li>
+          <><div key={request.id} className="bg-white my-4 p-4 rounded shadow-md">
+           <li className='font-bold'>{request.sender ? `${request.sender.username} - Pending` : 'Unknown Sender'}</li> 
+           <div className="flex space-x-4">
+            <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600" onClick={() => handleAddFriend(request.sender.id, request.id)}>Accept</button>
+          <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600" onClick={() => handleRejectRequest(request.id, request.sender.username)}>Reject</button>
+          </div>
+          </div></>
         ))}
       </ul>
     </div>
